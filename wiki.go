ac4acf9c4d1bd0d8503365b9bda4680d4e4d05ec
ayan412 <mussatayev.ayan@gmail.com>
2023-05-10 08:56:52 +0600

    changed viewHandler

--- wiki.go
+++ wiki.go
@@ -28,7 +28,11 @@ func loadPage(title string) (*Page, error) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
@@ -52,3 +56,5 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	t, _ := template.ParseFiles(tmpl + ".html")
 	t.Execute(w, p)
 }
+
+
