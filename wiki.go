7612b8c8dd0125ab1cd77b007b319f71af096db7
gradus412 <mussatayev.ayan@gmail.com>
2023-05-11 11:42:19 +0600

    added func getTitle

--- wiki.go
+++ wiki.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
@@ -70,6 +71,15 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	// if err != nil {
 	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
 	// }
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil //The title is the second subexpression.
 }
 
 func main() {
